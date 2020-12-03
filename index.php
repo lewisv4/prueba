a8263963da333f482aedd17d161e68f5423110a0
lewis <lewisv4re@gmail.com>
2020-12-02 20:22:30 -0500

    inicio

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div>
+    <p>
+        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore placeat illum natus nulla ipsam? At, sed laboriosam, earum quae dicta ut non dolor quam atque distinctio qui suscipit error! In.</span>
+        <span>Hic illum numquam placeat soluta reprehenderit qui reiciendis fuga pariatur voluptas explicabo dolorum, corrupti aspernatur quisquam, suscipit repudiandae similique alias commodi? Ipsum nihil odit, quia explicabo excepturi blanditiis omnis voluptates.</span>
+        <span>Quam quod atque alias! Iure illo quidem cumque modi nobis placeat nulla vitae nisi tempora voluptatum! Aspernatur, placeat minus. Exercitationem, magni enim itaque cum omnis reprehenderit hic possimus ipsum expedita.</span>
+        <span>Eius eaque debitis atque voluptatum assumenda dignissimos tempore quidem veritatis tempora, minima aperiam omnis iusto eos qui quae rerum cum deserunt vel, provident animi. Explicabo ipsum adipisci fuga magni quas.</span>
+        <span>Omnis ullam iure harum? Architecto blanditiis unde aut id fuga illum soluta nisi! Accusantium labore quibusdam soluta commodi. Debitis laudantium laborum eius, doloremque incidunt hic eligendi ab non cum? Quis.</span>
+        <span>Velit incidunt sed nostrum. Possimus, quia nobis? Tempore hic sint, omnis temporibus adipisci laudantium esse architecto quod, sit aut quidem quam quibusdam iste fuga voluptas repellat commodi vel atque reprehenderit!</span>
+        <span>Iste sint quasi aliquid saepe qui quod perspiciatis, expedita fugit aspernatur nesciunt nostrum voluptatibus culpa. Debitis consectetur saepe assumenda ullam velit placeat culpa id, enim, adipisci amet reiciendis qui nemo!</span>
+        <span>Deserunt voluptatem architecto ullam. Quis magnam voluptas ratione ex quam veritatis blanditiis quae vero, optio architecto! Inventore molestiae eius, tenetur tempore, totam libero minus eveniet quia reprehenderit impedit ex error?</span>
+        <span>Laborum nobis repellendus asperiores rem. Neque quaerat eaque consectetur pariatur in, deleniti, explicabo, unde dolore minima laborum distinctio molestias? Provident reprehenderit similique recusandae inventore illo debitis adipisci eligendi pariatur nobis?</span>
+        <span>Labore nulla ex consequatur ratione? Iusto veniam minus inventore vitae reiciendis ipsam voluptatem, dolorem cum, neque laudantium illo hic deserunt vero accusamus nobis eligendi. Esse nostrum exercitationem eveniet debitis ipsa.</span>
+    </p>
+</div>
+
+    <button>
+    
+    </button>
+</body>
+</html>
---
+++ prueba1.php
@@ -0,0 +1,3 @@
+<div></div>
+<p></p>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet enim, sequi accusantium veritatis aut quo! Unde fugiat officiis vitae facilis cupiditate tempore nemo ipsum, ab aspernatur! Officia enim delectus non!
